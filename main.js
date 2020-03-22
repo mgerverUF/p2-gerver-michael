7bfb442e9eeefe45c801b54d5f10940b88850d15
Michael Gerver <mgerver@ufl.edu>
2020-03-22 14:33:34 -0400

    Add code to access and print API data to visit page

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+var dayOfWeekContainer = document.getElementById('day-of-week')
+console.log(dayOfWeekContainer);
+
+var xmlhttp = new XMLHttpRequest();
+
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+
+        // This is your response
+        var apiResult = JSON.parse(this.responseText);
+
+        dayOfWeekContainer.innerHTML = apiResult.day_of_week;
+    }
+};
+xmlhttp.open('GET', 'http://worldtimeapi.org/api/ip', true);
+xmlhttp.send();
+
+
+
+
+/*
 var readMore = document.getElementById('expand-button');
 var moreText = document.getElementById('more-text');
 
@@ -7,7 +28,9 @@ function showText() {
     document.getElementById('more-text').classList.remove('hide');
 };
 
-/*
+
+
+
 readMore.addEventListener('click', showText)
 function showText() {
     if(moreText.style.display == 'none')
