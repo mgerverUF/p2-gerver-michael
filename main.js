69235f750c23c45f7f3da6c8b920f6206a093371
Michael Gerver <mgerver@ufl.edu>
2020-03-22 18:54:25 -0400

    Add accordion functionality to visit page

--- main.js
+++ main.js
@@ -15,25 +15,25 @@ xmlhttp.onreadystatechange = function() {
         // If statements to convert weekday number to weekday string        
         // https://stackoverflow.com/questions/41147371/changing-innerhtml-based-on-input-value
         if (dayOfWeekContainer.innerHTML == 0) {
-            dayOfWeekContainer.innerHTML = 'Sunday';
+            dayOfWeekContainer.innerHTML = 'Sunday, so we are closed.';
             }
         if (dayOfWeekContainer.innerHTML == 1) {
-            dayOfWeekContainer.innerHTML = 'Monday';
+            dayOfWeekContainer.innerHTML = 'Monday, so we are open from 9am - 5pm.';
             }   
         if (dayOfWeekContainer.innerHTML == 2) {
-            dayOfWeekContainer.innerHTML = 'Tuesday';
+            dayOfWeekContainer.innerHTML = 'Tuesday, so we are open from 9am - 5pm.';
             }
         if (dayOfWeekContainer.innerHTML == 3) {
-            dayOfWeekContainer.innerHTML = 'Wednesday';
+            dayOfWeekContainer.innerHTML = 'Wednesday, so we are open from 9am - 5pm.';
             }  
         if (dayOfWeekContainer.innerHTML == 4) {
-            dayOfWeekContainer.innerHTML = 'Thursday';
+            dayOfWeekContainer.innerHTML = 'Thursday, so we are open from 9am - 5pm.';
             }   
         if (dayOfWeekContainer.innerHTML == 5) {
-            dayOfWeekContainer.innerHTML = 'Friday';
+            dayOfWeekContainer.innerHTML = 'Friday, so we are open from 9am - 5pm.';
             }
         if (dayOfWeekContainer.innerHTML == 6) {
-            dayOfWeekContainer.innerHTML = 'Saturday';
+            dayOfWeekContainer.innerHTML = 'Saturday, so we are closed.';
             }               
     }
 };
@@ -42,8 +42,22 @@ xmlhttp.open('GET', 'http://worldtimeapi.org/api/ip', true);
 xmlhttp.send();
 
 
-
-
+// START CODE FOR ACCORDION
+// https://codepen.io/sureshrkm/pen/ZbzBpr 
+var accItem = document.getElementsByClassName('hours-menu');
+    var accHD = document.getElementsByClassName('hours-menu-heading');
+    for (i = 0; i < accHD.length; i++) {
+        accHD[i].addEventListener('click', toggleItem, false);
+    }
+    function toggleItem() {
+        var itemClass = this.parentNode.className;
+        for (i = 0; i < accItem.length; i++) {
+            accItem[i].className = 'hours-menu close';
+        }
+        if (itemClass == 'hours-menu close') {
+            this.parentNode.className = 'hours-menu open';
+        }
+    }
 
 /*
 var readMore = document.getElementById('expand-button');
