ccd51ca6a4bef87b349d6a20cb0ce16a4ee3d4b8
Michael Gerver <mgerver@ufl.edu>
2020-03-29 23:34:34 -0400

    Add javascript code for read more button

--- main.js
+++ main.js
@@ -59,29 +59,23 @@ var accItem = document.getElementsByClassName('hours-menu');
         }
     }
 
-/*
+
+// START CODE FOR READ MORE/READ LESS FUNCTIONALITY
+// https://codepen.io/shimaamohamed923/pen/yLyaLKv
 var readMore = document.getElementById('expand-button');
 var moreText = document.getElementById('more-text');
 
 readMore.addEventListener('click', showText)  
 function showText() {
-    document.getElementById('more-text').classList.add('show');
-    document.getElementById('more-text').classList.remove('hide');
-};
+        if(moreText.classList.contains('hide')) {
+          moreText.classList.remove('hide');
+          moreText.classList.add('show');
+          readMore.innerHTML = 'Read Less';
+        }
 
-
-
-
-readMore.addEventListener('click', showText)
-function showText() {
-    if(moreText.style.display == 'none')
-    {
-    moreText.style.display('block');
-    }
-
-    else {
-    moreText.classList.add('hide');
-    moreText.classList.remove('show');
-    }
-};
-*/
+        else if(moreText.classList.contains('show')) {
+          moreText.classList.remove('show');
+          moreText.classList.add('hide');
+          readMore.innerHTML = 'Read More';
+        }
+    };    
