2c3f0467b52165cfb43c9f6e488af7740b3ef9d6
Michael Gerver <mgerver@ufl.edu>
2020-03-30 20:40:40 -0400

    Add javascript code for pop-up

--- main.js
+++ main.js
@@ -81,5 +81,14 @@ function showText() {
     };    
 
 
+
 // START CODE FOR HOMEPAGE POP-UP
-var popUp = document.getElementById('');    
+var popUp = document.getElementById('text-window'); 
+var overlay = document.getElementById('overlay');
+var closeButton = document.getElementById('close-pop-up');
+
+closeButton.addEventListener('click', hidePopUp)
+function hidePopUp() {
+    popUp.classList.add('hide');
+    overlay.classList.add('hide');
+};   
